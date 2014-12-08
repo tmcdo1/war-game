2509f200abb8bdaa14bfa0f67f6be10bc5878a66
StrawnSC <strawnsc@gmail.com>
2014-12-08 14:51:10 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,42 @@
 public class Player
 {
-	public Player()
+	private ArrayList<Card> hand;// represents the cards a player has in his/her hand 
+	private int score; // This is the number that represents the amount of tricks won and can only be added onto
+	private String name; // the name a player has input to represent themselves
+	private boolean inGame; //if a player is still in the game, this value is true, if they are eliminated it is false
+	public Player(String n) //Constructs a player with name n 
 	{
+	}
+	public Card deal()// Returns the bottom card from hand and deletes it
+	{
+		
+	}
+	public Card deal(int n)// Returns the bottom n cards from hand and deletes them
+	{
+		
+	}
+	public void addToHand(Card c)//adds one card c to the top of the hand
+	{
+		
+	}
+	public String displayHand() // prints the contents of hand in an asthetically pleasing way
+	{
+		
+	}
+	public int getNumCards()// returns the number of cards in hand
+	{
+		return hand.size();
+	}
+	public boolean getInGame()
+	{
+		return inGame;
+	}
+	public int getScore() 
+	{
+		return score; 
+	}
+	public String getName()
+	{
+		return name; 
 	}
 }
