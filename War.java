349f1bf1ffc5c1f46d703f1d7b37b7dd408a1431
StrawnSC <strawnsc@gmail.com>
2014-12-08 14:37:02 -0600

    Update War.java

--- War.java
+++ War.java
@@ -1,7 +1,7 @@
 public class War
 {
 	private int numPlayers; //number of players
-	private Scanner scan = new Scanner(System.in);
+	private Scanner scan = new Scanner(System.in); //takes user input
 	public static void main(String[] args)
 	{
 	}
