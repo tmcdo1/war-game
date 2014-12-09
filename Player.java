8f5588c797ce99cd4d18be27a4f2775059741ec3
tmcdo1 <tmcdonald1234@yahoo.com>
2014-12-09 14:35:47 -0600

    Update Player

--- Player.java
+++ Player.java
@@ -7,25 +7,26 @@ public class Player
 	public Player(String n) //Constructs a player with name n 
 	{
 		name = n; 
+		score = 0; //sets default score. Sets to 0 wins
 	}
 
-	public Card deal()// Returns the bottom card from hand and deletes it
+	public Card deal()// Returns the top card from hand and deletes it
 	{
-		Card playedCard = hand.get(hand.size()-1)
-		hand.remove(hand.size()-1)
+		Card playedCard = hand.get(0);
+		hand.remove(0);
 		return playedCard;
 	}
-	public Card deal(int n)// Returns the bottom n cards from hand and deletes them
+/*	public Card deal(int n)// Returns the bottom n cards from hand and deletes them
 	{
-		
-	}
+
+*/	}
 	public void addToHand(Card c)//adds one card c to the top of the hand
 	{
-		hand.add(0,c);
+		hand.add(c);
 	}
 	public String displayHand() // prints the contents of hand in an asthetically pleasing way
 	{
-		
+		return hand;
 	}
 	public int getNumCards()// returns the number of cards in hand
 	{
