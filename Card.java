0b7d55a021f485cc47a8ee9696d80255a1bb8e1f
StrawnSC <strawnsc@gmail.com>
2014-12-08 21:15:11 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,13 +1,13 @@
 public class Card
 {
-  private int suitVal; 
-  private int value; 
-  private int rankVal; 
-  private int points; 
-  private String rankStr;
-  private String suitStr; 
+  private int suitVal; //0-3 value, with spades, clubs, hearts, and diamonds respectively 
+  private int value; // 0-51 represents the unique card
+  private int rankVal; //0-12 from ace to king respectively 
+  private int points; // 2-14 from two to ace respectively
+  private String rankStr; // the string that shows the card’s rank in a word
+  private String suitStr; // the string that shows the card’s suit in a word
   
-  public Card (int val) 
+  public Card (int val) // contructs a card with value val
   {
     value = val;
   }
