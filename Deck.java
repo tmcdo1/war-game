302427c785f9f20fdb50ce209fcc450c35dea942
StrawnSC <strawnsc@gmail.com>
2014-12-09 12:53:59 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -2,4 +2,20 @@ public class Deck
 {
   private ArrayList<Card> deck; //contains Card objects that make up the deck for the game
   
+  public Deck() //
+  {
+    
+  }
+  public Deck(int n) //
+  {
+    
+  }
+  public Deck shuffle (Deck d) //
+  {
+    
+  }
+  public Card deal() //
+  {
+    
+  }
 }
