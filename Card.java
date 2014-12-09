25842ccc608b1a4f3cd32d2dc708d6d09db4a33f
StrawnSC <strawnsc@gmail.com>
2014-12-08 21:37:31 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,10 +6,40 @@ public class Card
   private int points; // 2-14 from two to ace respectively
   private String rankStr; // the string that shows the card’s rank in a word
   private String suitStr; // the string that shows the card’s suit in a word
+  private String[] asciiLines = new String[6];//The string lines that when printed together on top of each other 
+                                              //produce the ascii representation of the card
   
   public Card (int val) // contructs a card with value val
   {
     value = val;
+    setAsciiLines(); 
+  }
+  private setAsciiLines() // sets up the lines of Ascii art to visualy represent a card, showing rank and suit
+  {
+    asciiLines[0] = ".------.";
+    asciiLines[1] = "|"+rankStr+".--. |";
+    if (suitVal==0) //If the card is a spade
+      {
+      asciiLines[2] = "| :/\: |";
+      asciiLines[3] = "| (__) |";
+      }
+      if (suitVal==1) //If the card is a club
+      {
+      asciiLines[2] = "| :(): |";
+      asciiLines[3] = "| ()() |";
+      }
+      if (suitVal==2) //If the card is a heart
+      {
+      asciiLines[2] = "| (\/) |";
+      asciiLines[3] = "| :\/: |";
+      }
+      if (suitVal==3) //If the card is a diamond
+      {
+      asciiLines[2] = "| :/\: |";
+      asciiLines[3] = "| :\/: |";
+      }
+    asciiLines[4] = "| '--'"+rankStr+"|";
+    asciiLines[5] = "`------'";
   }
   public toString() //returns a string representation of a card, with points, rank, and suit
                    //seperate from the ascii-art represenation 
