e17bafd3247b57085ad0995b30bb294287ee1761
tmcdo1 <tmcdonald1234@yahoo.com>
2014-12-10 14:53:49 -0600

    Update

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ public class Game
 	private int numPlayers; //number of players in the game
 	private ArrayList<Player> players; //a list of all the players in the game
 	private ArrayList<Card> compared; //the cards that are on the table for each trick and are compared
-	private ArrayList<Card> pool; //the cards that are not compared in a game of war but are still won
+	private ArrayList<Card> pot; //the cards that are not compared in a game of war but are still won
 	
 	public Game()
 	{
@@ -25,7 +25,7 @@ public class Game
 			compared.add(players.get(h).deal());
 		}
 		
-
+//winner wins cards in pot
 	}
 	public void war()
 	{
