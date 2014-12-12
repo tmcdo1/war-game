e91acd545c554634a1bd2eab0d118500769040dd
tmcdo1 <tmcdonald1234@yahoo.com>
2014-12-12 14:56:40 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -49,16 +49,20 @@ System.out.println("Welcome to the Game of War");
 //display the cards in the compare ArrayList
 		displayTable();
 
-		index = compare(compared);
+		index = compare(compared,0);
 //winner wins cards in pot if there is a pot
 		if(pot.size()!=0)
-			for(int h = 0; h<pot.size();h++)
+			for(int h = pot.size()-1; h>+0;h++)
 			{
 				players.get(index).addToHand(pot.get(h));
+				pot.remove(h);
 			}
 //winner wins the cards on the table
 		for(int h = 0; h<compared.size();h++)
 			players.get(index).addToHand(compared.get(h));
+//Prints who won the trick
+		System.out.println("The winner of the trick is ---> "+getTrickWinner().getName());
+		System.out.println();
 //checks to see if players are out of cards and removes them if they are
 		for(int h = 0; h<players.size();h++)
 		{
@@ -67,17 +71,29 @@ System.out.println("Welcome to the Game of War");
 		}
 //displays the hand for each player
 		for(int h = 0; h<players.size();h++)
+		{
+			System.out.print(players.get(h).getName()+"'s hand: ");
 			players.get(h).displayHand();
+		}
 		//waits for the user to respond
+		System.out.println();
 		System.out.println("Please press <Enter> to continue");
 		String dummy = input.nextLine();
 	}
 	private void war() //runs when there is a tie in high value cards
 	{
-		for(int h = 0; h<compared.size();h++) //takes existing ArrayList<Card> compared and puts those Cards in the pot ArrayList
+//
+		for(int h = 0; h<players.size();h++)
 		{
-			pot.add(compared.get(0));
-			compared.remove(0);
+			System.out.print(players.get(h).getName()+"'s hand: ");
+			players.get(h).displayHand();
+		}
+
+//
+		for(int h = compared.size()-1; h>=0;h--) //takes existing ArrayList<Card> compared and puts those Cards in the pot ArrayList
+		{
+			pot.add(compared.get(h));
+			compared.remove(h);
 		}
 		Card x;
 		for(int i = 0; i<3;i++) //players deal cards and it adds to the pot
@@ -103,51 +119,71 @@ System.out.println("Welcome to the Game of War");
 				h--;
 			}
 		}
+//
+		for(int h = 0; h<players.size();h++)
+		{
+			System.out.print(players.get(h).getName()+"'s hand: ");
+			players.get(h).displayHand();
+		}
+//
 		displayTable();
+
 	}
 	public void addPlayer(Player a) //adds a player to the players ArrayList
 	{
 		players.add(a);
 	}
-	public int compare(ArrayList<Card> c) //compares the cards in the compared ArrayList and returns the index of the highest card. Calls on war()
+	public int compare(ArrayList<Card> c, int i) //compares the cards in the compared ArrayList and returns the index of the highest card. Calls on war()
 	{
 		int x = 0;
 		for(int h=0;h<c.size()-1;h++)
 		{
-			if(c.get(index).getPoints()<=c.get(h).getPoints())
+			if(c.get(x).getPoints()<=c.get(h).getPoints())
 				x=h;
 		}
-		if(isTie()!=true)
+		i=x;
+		if(isTie(c,i) == false)
+		{
+
 			return x;
+		}
 		else
 		{
+			System.out.println("Prepare for war");
+			System.out.println();
 			war();
-			return this.compare(compared);
+			return this.compare(c,i);
 		}
 
 
 
 	}
-	public boolean isTie() //checks to see if there is a tie in high values
+	public boolean isTie(ArrayList<Card> c, int y) //checks to see if there is a tie in high values
 	{
+
 		int x = 0;
-		for(int h=0;h<compared.size();h++)
+		for(int h=0;h<c.size();h++)
 		{
-			if(compared.get(index)==compared.get(h))
-			{
-				if(index!=h)
-					x=1;
-			}
+			if(y!=h)
+				if(c.get(y).getPoints()==c.get(h).getPoints())
+				{
+						x++;
+				}
 		}
-		if(x==1)
+		if(x>=1)
+		{
 			return true;
+		}
 		else
+		{
 			return false;
+		}
 	}
 	private void displayTable()//displays cards in the compared ArrayList()
 	{
-		for(int h=0;h<compared.size();h++)
+		for(int h=0;h<players.size();h++)
 		{
+			System.out.println(players.get(h).getName());
 			String[] lines = compared.get(h).getAsciiLines();
 			for(int i =0;i<lines.length;i++)
 				System.out.println(lines[i]);
@@ -169,5 +205,9 @@ System.out.println("Welcome to the Game of War");
 	public ArrayList<Player> getPlayers()
 	{
 		return players;
+	}
+	public Player getTrickWinner()
+	{
+		return players.get(index);
 	}
 }
